21ff4ce8d49ebad5cf8e9e3b994772312f50759f
Jatin Uppal <jatinuppal89@gmail.com>
2019-04-22 02:06:45 +0530

    Stack with push operation added.

---
+++ project.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std;
+
+class Stack
+ {
+	int top;
+	int size;
+	int *stack;
+
+      public:
+	Stack();
+	void push( int );
+	void pop();
+	void display();
+	void menu();
+ };
+
+void Stack::menu()
+ {
+	 int k,a ;
+	 cout << "\n What do you want to do :" ;
+	 do
+	   {
+	         cout << "\n1. Push";
+	         cout << "\n Press 0 to leave." ;
+		 cout << endl <<"\nYour choice - " ;
+		 cin >> k ;
+		 switch( k )
+		       {
+			       case 0 :
+				       cout << "\nOut of Stack" ;
+				    break ;
+			       case 1 :
+				       cout << endl << "\nEnter the value - " ;
+				       cin >> a ;
+				       push( a ) ;
+				    break ;
+			       default :
+				    cout << "\nInvalid Input" ;
+		       }
+	   
+	   }  while( k ) ;
+ }
+
+ Stack::Stack()
+  {
+	  top = -1 ;
+	  cout << endl << "\nEnter size of stack - " ;
+	  cin >> size ;
+	  stack  = new int[ size ] ;
+  }
+
+void Stack::push( int data )
+ {
+	 if( top == size-1 )
+		 cout << "\nStack Overflow !" ;
+	 else
+	  {
+		 stack[ ++top ] = data ;
+		 cout << endl << stack[ top ] <<" added to the stack." ;
+	  }
+ }
+
+int main()
+ {
+	int n ;
+	cout << "This program can implement following structures :" ;
+	do 
+	 {
+	     cout << "\n1. Stack ( of integers )" ;      // in beginning only stack added but later more options will be added.
+	     cout << "\n Press 0 for exit." ;
+	     cout << endl << "\nEnter your choice - " ;
+	     cin >> n ;
+	     switch( n )
+		   {
+		   	   case 0 :
+		   	           cout << "\nThank You for visiting our program." ;
+		   	           exit(0) ;    // this infinite loop will terminate here.
+		           case 1 :
+				   {
+		   	             Stack s1 ;
+		   	             s1.menu() ;
+				   }
+                                 break ;
+		           default :
+		   	           cout << "\nInvalid Input" ;
+	           }
+
+	} while( 1 ) ;
+
+ }
