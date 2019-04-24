811e441adc8776b267251640616c163bdf8df95f
Hridyanshu <hridyanshusharma@gmail.com>
2019-04-24 13:19:06 +0530

    Added insertion fuction for the beginning

--- project.cpp
+++ project.cpp
@@ -129,6 +129,20 @@ void circular_llist::create_node(int value)
         last = temp;
     }
 
+    void circular_llist::add_begin(int value)
+{
+    if (last == NULL)
+    {
+        cout<<"First Create the list."<<endl;
+        return;
+    }
+    struct node *temp;
+    temp = new(struct node);
+    temp->info = value;
+    temp->next = last->next;
+    last->next = temp;
+}
+
 int main()
  {
 	int n ;
